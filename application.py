0d1714bc9d7c8125b54bbd0996f3e1dafebe2961
AndersSaints <anders.lsantos@gmail.com>
2023-10-11 14:51:43 +0100

    Fixed db access

--- application.py
+++ application.py
@@ -11,8 +11,6 @@ db = SQLAlchemy(app)
 # >>> from project import app, db
 # >>> app.app_context().push()
 # >>> db.create_all()
-with app.app_context:
-    db.create_all()
 
 
 class Drink(db.Model):
