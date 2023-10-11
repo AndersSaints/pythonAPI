c2bec0034e5161df1758231a0cf2893f0b5693ab
AndersSaints <anders.lsantos@gmail.com>
2023-10-11 15:05:56 +0100

    Corrected DELETE endpoint

--- application.py
+++ application.py
@@ -52,3 +52,13 @@ def add_drink():
     db.session.add(drink)
     db.session.commit()
     return {'id': drink.id}
+
+
+@app.route('/drinks/<id>', methods=['DELETE'])
+def delete_drink(id):
+    drink = Drink.query.get(id)
+    if drink is None:
+        return {"Error": "Drink not found"}
+    db.session.delete(drink)
+    db.session.commit()
+    return {"Drink deletion": "Successful"}
