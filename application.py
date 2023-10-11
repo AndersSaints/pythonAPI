2050ed62f4c0a1deb11c974a56d54a526032c2ec
AndersSaints <anders.lsantos@gmail.com>
2023-10-11 13:59:55 +0100

    Update app

--- application.py
+++ application.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
 
 class Drink(db.Model):
     id = db.Column(db.Integer, primary_key=True)
